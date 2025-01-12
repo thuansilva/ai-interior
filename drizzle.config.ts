0b2e26c2d14c30523477a2820ac170e3fe689145
Thuan Silva <thuansilva14@gmail.com>
2025-01-12 17:09:02 -0300

    Remove chave privada do código

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: "./config/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: "postgresql://interiodb_owner:Wj0BTQKCzFG8@ep-wandering-flower-a5ak0tq5.us-east-2.aws.neon.tech/interiodb?sslmode=require",
+    url: process.env.DATABASE_URL!,
   },
 });
